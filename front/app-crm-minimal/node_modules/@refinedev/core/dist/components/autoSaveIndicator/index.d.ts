1ff7c36f7f144091f407b8172cbe1b5f56e0ca37
kuyeol <125532184+kuyeol@users.noreply.github.com>
2023-11-30 09:44:18 +0900

    clean

--- front/app-crm-minimal/node_modules/@refinedev/core/dist/components/autoSaveIndicator/index.d.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { AutoSaveIndicatorProps } from "../../interfaces";
-export declare const AutoSaveIndicator: React.FC<AutoSaveIndicatorProps>;
-//# sourceMappingURL=index.d.ts.map
